8830fe9bf5014cac523e024754976dbc83af372b
Arthur <gyulabyana@gmail.com>
2021-07-21 03:15:57 +0400

    feat(App): apply minor styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex place-content-center">
       <Todo />
-      <div className="p-6 items-center justify-center">
-        <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
-        <p className="tracking-widest">This is my first React App.</p>
-      </div>
     </div>
   );
 }
