e011198ad58a0eeb6ac387a82236247f5df6ed62
Arthur <gyulabyana@gmail.com>
2021-07-21 01:07:22 +0400

    test(*): test tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import Todo from './components/Todo/Todo';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-
+    <div className="App">
+      <Todo />
+      <div className="p-6 items-center justify-center">
+        <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
+        <p className="tracking-widest">This is my first React App.</p>
+      </div>
     </div>
   );
 }
