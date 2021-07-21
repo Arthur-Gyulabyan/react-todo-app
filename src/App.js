4e45ebccf03090257bb6ebd5305932f74aa6426f
Arthur <gyulabyana@gmail.com>
2021-07-22 03:12:18 +0400

    test: testing classnames function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 import Todo from './components/Todo/Todo';
 import './App.css';
 
 function App() {
+  const appClassnames = classNames({
+    flex: true,
+    'place-content-center': true,
+    'mt-10': true,
+  });
   return (
-    <div className="flex place-content-center">
+    <div className={appClassnames}>
       <Todo />
     </div>
   );
