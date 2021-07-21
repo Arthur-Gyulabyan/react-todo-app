88296320ed0c5507098cf9d19f7fb90ff083f5d2
Arthur <gyulabyana@gmail.com>
2021-07-21 18:31:59 +0400

    feat(Todo): add inputChangeHandler and update addItem methods

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import classNames from 'classnames';
 import TodoItem from '../TodoItem/TodoItem';
+import Input from '../Input/Input';
+import getUniqueId from '../../helpers/idGenerator';
 
 export default class Todo extends React.Component {
   constructor(props) {
@@ -8,32 +10,40 @@ export default class Todo extends React.Component {
 
     this.state = {
       items: [],
+      currentText: '',
     };
   }
 
-  addItem = e => {
-    if (e.key === 'Enter') {
-      const text = e.target.value;
+  addItem = (e, currentText) => {
+    if (e.key === 'Enter' || e.type === 'click') {
+      const id = getUniqueId();
+
       this.setState(prevState => {
-        return { items: [...prevState.items, { text }] };
+        return {
+          items: [...prevState.items, { text: currentText, id }],
+        };
       });
     }
   };
 
+  inputChangeHandler = e => {
+    this.setState({ currentText: e.target.value });
+  };
+
   render() {
-    const { items } = this.state;
-    console.log(items);
+    const { items, currentText } = this.state;
 
     return (
       <div>
-        <input
-          onKeyPress={this.addItem}
-          type="text"
+        <Input
           placeholder="What you should do..."
+          enterHandler={this.addItem}
+          changeHandler={this.inputChangeHandler}
+          currenText={currentText}
         />
-        <ul className="list-disc">
+        <ul>
           {items.map(item => {
-            return <TodoItem text={item.text} />;
+            return <TodoItem text={item.text} key={item.id} />;
           })}
         </ul>
       </div>
