02a8898659f4b0d913292a702cc81366e056f926
Arthur <gyulabyana@gmail.com>
2021-07-21 18:32:38 +0400

    feat(TodoItem): create TodoItem component

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 // import classNames from 'classnames';
 
-export default function TodoItem({ text }) {
-  return <li>{text}</li>;
+export default function TodoItem({ text, id }) {
+  return (
+    <li className="flex justify-between" id={id}>
+      <p>{text}</p>
+
+      <div>
+        <button type="button">Done</button>
+        <button type="button">Edit</button>
+        <button type="button">Delete</button>
+      </div>
+    </li>
+  );
 }
