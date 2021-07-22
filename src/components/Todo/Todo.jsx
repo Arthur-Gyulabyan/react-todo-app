07f8da91f195089788b6cb2ae26d4b0828a2ef7c
Arthur <gyulabyana@gmail.com>
2021-07-23 01:12:31 +0400

    feat(Todo): add item edit functionality

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -23,7 +23,7 @@ export default class Todo extends React.Component {
           return {
             items: [
               ...prevState.items,
-              { text: currentText, id, isCompleted: false },
+              { text: currentText, id, isCompleted: false, isEditable: false },
             ],
             currentText: '',
           };
@@ -40,6 +40,31 @@ export default class Todo extends React.Component {
     this.setState({ items });
   };
 
+  toggleEdit = (e, id) => {
+    const { items } = this.state;
+    const index = items.findIndex(el => el.id === id);
+
+    items[index].isEditable = !items[index].isEditable;
+
+    this.setState({ items });
+  };
+
+  editItem = (e, id, text) => {
+    if (e.key === 'Enter' || e.type === 'click') {
+      const { items } = this.state;
+      const index = items.findIndex(el => el.id === id);
+
+      if (text === '') {
+        this.deleteItem(e, id);
+        return;
+      }
+
+      items[index].text = text;
+      this.toggleEdit(e, id);
+      this.setState({ items });
+    }
+  };
+
   deleteItem = (e, id) => {
     const { items } = this.state;
     const newItems = items.filter(el => el.id !== id);
@@ -53,6 +78,14 @@ export default class Todo extends React.Component {
 
   inputChangeHandler = e => {
     this.setState({ currentText: e.target.value });
+  };
+
+  editInputChangeHandler = (e, id) => {
+    const { items } = this.state;
+    const index = items.findIndex(el => el.id === id);
+
+    items[index].text = e.target.value;
+    this.setState({ items });
   };
 
   render() {
@@ -78,8 +111,12 @@ export default class Todo extends React.Component {
                 text={item.text}
                 key={item.id}
                 checkHandler={e => this.toggleComplete(e, item.id)}
+                editHandler={e => this.toggleEdit(e, item.id)}
+                editInputHandler={e => this.editInputChangeHandler(e, item.id)}
+                editItem={e => this.editItem(e, item.id, item.text)}
                 deleteHandler={e => this.deleteItem(e, item.id)}
                 isCompleted={item.isCompleted}
+                isEditable={item.isEditable}
               />
             );
           })}
